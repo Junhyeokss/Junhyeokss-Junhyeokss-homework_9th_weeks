2e97643c78a6b7aea5f02a24272663e46e7733a4
Junhyeokss <118700002+Junhyeokss@users.noreply.github.com>
2023-01-16 17:41:20 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".main_slide").slick({
+    arrows: true,
+  });
+  $(".m_link li a").on("click", function (e) {
+    e.preventDefault();
+    var idx = $(this).parent().index();
+    $(".main_slide").slick("slickGoTo", idx);
+  });
+});
