eb84c3d1ddf8030cff76f1df7a41a1986868b7b8
Junhyeokss <118700002+Junhyeokss@users.noreply.github.com>
2023-01-19 17:50:21 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,4 +11,31 @@ $(function () {
     var idx = $(this).parent().index();
     $(".main_slide").slick("slickGoTo", idx);
   });
+
+  $("#gnb>ul>li>a").on("click", function (e) {
+    e.preventDefault();
+    $(this).next().slideToggle();
+    //$(this).next().toggle();
+  });
+
+  $(".gnb>ul>li>a").on("click", function (e) {
+    e.preventDefault();
+    $(this).next().slideToggle();
+    //$(this).next().toggle();
+  });
+
+  $(".second_inner #gnb > ul > li .xi-angle-down-min").on("click", function () {
+    //$('.fl ul').toggle();
+    //$('.fl ul').slideToggle();
+    $(this).toggleClass("on");
+
+    // $(this) : 내가 클릭한 것
+  });
+  $(".gnb > ul > li .xi-angle-down-min").on("click", function () {
+    //$('.fl ul').toggle();
+    //$('.fl ul').slideToggle();
+    $(this).toggleClass("on");
+
+    // $(this) : 내가 클릭한 것
+  });
 });
